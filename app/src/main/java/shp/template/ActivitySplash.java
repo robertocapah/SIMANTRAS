9b412ffe8f5c562bc2f1f60553d22b725df8fac5
robertocapah <roberto.capah@gmail.com>
2019-02-19 19:24:36 +0700

    splash

--- app/src/main/java/shp/template/ActivitySplash.java
+++ app/src/main/java/shp/template/ActivitySplash.java
@@ -166,7 +166,7 @@ public class ActivitySplash extends AppCompatActivity {
                         Manifest.permission.CAMERA)
                         ){
                     ActivityCompat.requestPermissions(ActivitySplash.this,
-                            new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CAMERA},
+                            new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CAMERA, Manifest.permission.READ_PHONE_STATE},
                             REQUEST_CODE_ASK_PERMISSIONS);
                     dialog.dismiss();
 
