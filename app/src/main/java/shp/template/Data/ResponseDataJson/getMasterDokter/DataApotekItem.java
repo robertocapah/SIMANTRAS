894fb0aff824ad9474be19d60c22b2082007cdf7
dewi9310 <dewioktaviani9310@gmail.com>
2019-02-28 11:03:57 +0700

    get master dokter model

---
+++ app/src/main/java/shp/template/Data/ResponseDataJson/getMasterDokter/DataApotekItem.java
@@ -0,0 +1,63 @@
+package shp.template.Data.ResponseDataJson.getMasterDokter;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("com.robohorse.robopojogenerator")
+public class DataApotekItem{
+
+	@SerializedName("code")
+	private String code;
+
+	@SerializedName("kecId")
+	private String kecId;
+
+	@SerializedName("name")
+	private String name;
+
+	@SerializedName("kecName")
+	private String kecName;
+
+	public void setCode(String code){
+		this.code = code;
+	}
+
+	public String getCode(){
+		return code;
+	}
+
+	public void setKecId(String kecId){
+		this.kecId = kecId;
+	}
+
+	public String getKecId(){
+		return kecId;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setKecName(String kecName){
+		this.kecName = kecName;
+	}
+
+	public String getKecName(){
+		return kecName;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"DataApotekItem{" + 
+			"code = '" + code + '\'' + 
+			",kecId = '" + kecId + '\'' + 
+			",name = '" + name + '\'' + 
+			",kecName = '" + kecName + '\'' + 
+			"}";
+		}
+}
