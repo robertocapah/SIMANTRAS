ce03916ab32090d2f412b112f37162226159b124
dewi.oktaviani <dewi.oktaviani@kalbenutritionals.com>
2019-06-20 13:48:28 +0700

    download file di detail transaction

--- app/src/main/java/com/kalbenutritionals/simantra/Fragment/FragmentTransactionDetail.java
+++ app/src/main/java/com/kalbenutritionals/simantra/Fragment/FragmentTransactionDetail.java
@@ -1,10 +1,13 @@
 package com.kalbenutritionals.simantra.Fragment;
 
 import android.accounts.AccountManager;
+import android.app.Dialog;
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
@@ -19,11 +22,17 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.androidnetworking.error.ANError;
+import com.github.lzyzsd.circleprogress.DonutProgress;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.kalbe.mobiledevknlibs.PickImageAndFile.UriData;
 import com.kalbe.mobiledevknlibs.ToastAndSnackBar.ToastCustom;
 import com.kalbenutritionals.simantra.ActivityLogin;
 import com.kalbenutritionals.simantra.ActivityMainMenu;
+import com.kalbenutritionals.simantra.ActivitySplash;
 import com.kalbenutritionals.simantra.BuildConfig;
 import com.kalbenutritionals.simantra.Data.ClsHardCode;
+import com.kalbenutritionals.simantra.Data.ResponseDataJson.filePDF.FilePDF;
 import com.kalbenutritionals.simantra.Data.ResponseDataJson.loginMobileApps.LoginMobileApps;
 import com.kalbenutritionals.simantra.Database.Common.ClsmUserLogin;
 import com.kalbenutritionals.simantra.Database.Repo.RepomUserLogin;
@@ -56,7 +65,10 @@ public class FragmentTransactionDetail extends Fragment {
     ImageButton btnDownload;
 
     Unbinder unbinder;
-    View v;;
+    View v;
+    private Gson gson;
+    private final String TAG_DOWNlOAD_FILE = "Download_file";
+    Dialog dialog;
 
 
     @Nullable
@@ -64,75 +76,20 @@ public class FragmentTransactionDetail extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_transaction_detail, container, false);
         unbinder = ButterKnife.bind(this, v);
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gson = gsonBuilder.create();
+        dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.layout_progress_download_apk);
+        dialog.setCancelable(false);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        final DonutProgress progressD = (DonutProgress) dialog.findViewById(R.id.progressPercentage);
+
         btnDownload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 downloadFile();
-//                Toast.makeText(getContext(), "Haiii", Toast.LENGTH_SHORT).show();
-//                new FastNetworkingUtils().FNRequestDownloadAPKFile(getActivity(), txtLink, txtPathUserData, apkName, TAG_DOWNlOAD_APK, dialog, new InterfaceFastNetworkingDownloadFile() {
-//                    @Override
-//                    public void onProgress(final long bytesDownloaded, final long totalBytes) {
-//                        // do anything with progress
-//                        runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                double precentage = ((double) bytesDownloaded / (double) totalBytes) * 100;
-//                                progressD.setProgress((int) precentage);
-////                                tvProgress.setText("bytes downloaded :" + bytesDownloaded + ". total bytes : " + totalBytes + " precentage : " + precentage + " %");
-//                            }
-//                        });
-//                    }
-//
-//                    @Override
-//                    public void onDownloadComplete() {
-//                        runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                dialog.dismiss();
-//                                Toast.makeText(getApplicationContext(), "Download Complete", Toast.LENGTH_SHORT).show();
-//                                String txtPath = txtPathUserData + apkName;
-//                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//                                    Toast.makeText(getApplicationContext(), "File downloaded", Toast.LENGTH_LONG).show();
-//                                    try {
-//                                        Intent intent = new Intent(Intent.ACTION_INSTALL_PACKAGE);
-//                                        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-//                                        File file = new File(txtPath);
-//                                        Uri uri = FileProvider.getUriForFile(getApplicationContext(), BuildConfig.APPLICATION_ID + ".provider", file);
-//                                        intent.setData(uri);
-//                                        startActivity(intent);
-//                                        finish();
-//                                    } catch (Exception e) {
-//                                        e.printStackTrace();
-//                                    }
-//                                } else {
-//                                    Toast.makeText(getApplicationContext(), "File downloaded", Toast.LENGTH_LONG).show();
-//                                    Intent intent = new Intent(Intent.ACTION_VIEW);
-//                                    intent.setDataAndType(Uri.fromFile(new File(txtPath)), "application/vnd.android.package-archive");
-//                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//
-//                                    if (Build.VERSION.SDK_INT >= 24) {
-//                                        intent.setDataAndType(FileProvider.getUriForFile(getApplicationContext(), getApplicationContext().getPackageName() + ".provider", new File(txtPath)), "application/vnd.android.package-archive");
-//                                        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-//                                    } else {
-//                                        intent.setDataAndType(Uri.fromFile(new File(txtPath)), "application/vnd.android.package-archive");
-//                                    }
-//                                    startActivity(intent);
-//                                    finish();
-//                                }
-//                            }
-//                        });
-//                    }
-//
-//                    @Override
-//                    public void onError(ANError error) {
-//                        runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                dialog.dismiss();
-//                            }
-//                        });
-//                    }
-//                });
+
             }
         });
         //detail pemeriksaan
@@ -148,63 +105,72 @@ public class FragmentTransactionDetail extends Fragment {
             public void onResponse(JSONObject response) {
 
                 if (response != null) {
-//                    final LoginMobileApps model = gson.fromJson(response.toString(), LoginMobileApps.class);
-//                    boolean txtStatus = model.getResult().isStatus();
-//                    String txtMessage = model.getResult().getMessage();
-//                    String txtMethode_name = model.getResult().getMethodName();
-//
-//
-//                    if (txtStatus == true) {
-//                        new Thread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                Intent res = null;
-//                                String accessToken = "dummy_access_token";
-//                                loginRepo = new RepomUserLogin(getApplicationContext());
-//                                ClsmUserLogin data = new ClsmUserLogin();
-//                                data.setTxtGuID(model.getData().getTxtGuiID());
-//                                data.setIntUserID(model.getData().getIntUserID());
-//                                data.setTxtUserName(model.getData().getTxtUserName());
-//                                data.setTxtNick(model.getData().getTxtNick());
-//                                data.setTxtEmpID(model.getData().getTxtEmpID());
-//                                data.setTxtEmail(model.getData().getTxtEmail());
-//                                data.setIntDepartmentID(model.getData().getIntDepartmentID());
-//                                data.setIntLOBID(model.getData().getIntLOBID());
-//                                data.setTxtCompanyCode(model.getData().getTxtCompanyCode());
-//                                if (model.getData().getMUserRole() != null) {
-//                                    data.setIntRoleID(model.getData().getMUserRole().getIntRoleID());
-//                                    data.setTxtRoleName(model.getData().getMUserRole().getTxtRoleName());
-//                                }
-//                                data.setDtLogIn(parseDate(model.getData().getDtDateLogin()));
-//
-//                                byte[] file = getByte(model.getData().getTxtLinkFotoProfile());
-//                                if (file != null) {
-//                                    data.setBlobImg(file);
-//                                } else {
-//                                    data.setBlobImg(null);
-//                                }
-//                                try {
-//                                    loginRepo.createOrUpdate(data);
-//                                } catch (SQLException e) {
-//                                    e.printStackTrace();
-//                                }
-//                                Log.d("Data info", "Login Success");
-//
-//                                datum.putString(AccountManager.KEY_ACCOUNT_NAME, txtUsername);
-//                                datum.putString(AccountManager.KEY_ACCOUNT_TYPE, accountType);
-//                                datum.putString(AccountManager.KEY_AUTHTOKEN, accessToken);
-//                                datum.putString(PARAM_USER_PASS, txtPassword);
-//                                datum.putString(ARG_AUTH_TYPE, mAuthTokenType);
-//                                datum.putBoolean(ARG_IS_ADDING_NEW_ACCOUNT, newAccount);
-//                                res = new Intent();
-//                                res.putExtras(datum);
-//                                finishLogin(res, mAccountManager);
-//                                Intent intent = new Intent(ActivityLogin.this, ActivityMainMenu.class);
-//                                finish();
-//                                startActivity(intent);
-//                            }
-//                        }).start();
+                    final FilePDF model = gson.fromJson(response.toString(), FilePDF.class);
+                    boolean txtStatus = model.getResult().isStatus();
+                    String txtMessage = model.getResult().getMessage();
+                    String txtMethode_name = model.getResult().getMethodName();
 
+                    if (model.getData()!=null){ 
+                        String txtLink = model.getData().getLinkFile();
+                        final String fileName = model.getData().getFileName();
+                        final String txtPathUserData = Environment.getExternalStorageDirectory() + File.separator + Environment.DIRECTORY_DOWNLOADS;
+//                        new UriData().getOutputMediaUriFolder(getContext(), txtPathUserData);
+//                        File mediaStorageDir = new File(txtPathUserData + File.separator);
+//                        // Create the storage directory if it does not exist
+//                        try {
+//                            File yourFile = new File(txtPathUserData);
+//                            if (!yourFile.exists()) {
+//                                if (yourFile.mkdirs()) {
+//                                    Log.d("DOWNLOAD File", "Successfully created the parent dir:" + yourFile.getName());
+//                                } else {
+//                                    Log.d("DOWNLOAD File", "Failed to create the parent dir:" + yourFile.getName());
+//                                }
+//                            }
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+                new FastNetworkingUtils().FNRequestDownloadAPKFile(getActivity(), txtLink, txtPathUserData, fileName, TAG_DOWNlOAD_FILE, dialog, new InterfaceFastNetworkingDownloadFile() {
+                    @Override
+                    public void onProgress(final long bytesDownloaded, final long totalBytes) {
+                        // do anything with progress
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                double precentage = ((double) bytesDownloaded / (double) totalBytes) * 100;
+//                                progressD.setProgress((int) precentage);
+//                                tvProgress.setText("bytes downloaded :" + bytesDownloaded + ". total bytes : " + totalBytes + " precentage : " + precentage + " %");
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onDownloadComplete() {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                dialog.dismiss();
+                                new ToastCustom().showToasty(getActivity(), "Download Complete", 1);
+                                // location = "/sdcard/my_folder";
+//                                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                                Uri mydir = new UriData().getOutputMediaUriFolder(getContext(), txtPathUserData);
+////                                intent.setDataAndType(mydir,"application/*");    // or use */*
+//                                intent.setDataAndType(mydir, "resource/folder");
+//                                startActivity(intent);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                dialog.dismiss();
+                            }
+                        });
+                    }
+                });
+                    }
                     } else {
                         new ToastCustom().showToasty(getActivity(), "gagal :(", 4);
                     }
